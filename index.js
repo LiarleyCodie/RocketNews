a59d25a35f959dc4bdde374a1764d557447d653c
liarleycodie <liarley.personal@gmail.com>
2023-03-25 00:37:19 -0300

    stage: Modal alert added but without trigger

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = {
+    element: document.querySelector("#submitButton"),
+    submit: function() {
+
+    }
+}
+
+const modal = {
+    element: document.querySelector("#modal"),
+    wrapper: document.querySelector("#modalWrapper")
+}
+
+button.element.onclick = button.submit
